dbbf0a22f34830e5f367a5120d52c6399324f4a0
wawah21 <43214146+wawah21@users.noreply.github.com>
2018-09-12 23:13:11 +0700

    Create sitemapDPT.js

---
+++ sitemapDPT.js
@@ -0,0 +1,43 @@
+<div dir="ltr" style="text-align: left;" trbidi="on">
+<style type="text/css">
+#toc{
+width:99%;
+margin:5px auto;
+border:1px solid #2D96DF;
+-webkit-box-shadow:4px 4px 8px 2px rgba(0,0,0, 0.2);
+-moz-box-shadow:4px 4px 8px 2px rgba(0,0,0, 0.2);
+box-shadow:4px 4px 8px 2px rgba(0,0,0, 0.2);
+}
+.labl{
+color:#FF5F00;
+font-weight:bold;
+margin:0 -5px;
+padding:1px 0 2px 11px;
+background:-moz-linear-gradient(right,#C2EAFE 0%,#055A85 40%);
+background:-webkit-gradient(linear,left 10,right 80,color-stop(0.20,#055A85),color-stop(1,#C2EAFE));
+border:1px solid #2D96DF;
+border-radius:4px;-moz-border-radius:4px;
+-webkit-border-radius:4px;box-shadow:3px 3px 1px #bbb;
+-moz-box-shadow:3px 3px 1px #bbb;-webkit-box-shadow:3px 3px 1px #bbb;display:block;
+}
+.labl a{
+color:#fff;
+}
+.labl:first-letter{t
+ext-transform:uppercase;
+}
+.new{
+color:#FF5F00;
+font-weight:bold;
+font-style:italic;
+}
+.postname{
+font-weight:normal;
+background:-moz-linear-gradient(right,#C2EAFE 0%,#fff 40%);
+background:-webkit-gradient(linear,left 80,right 10,color-stop(0.60,#fff),color-stop(1,#C2EAFE));
+}
+.postname li{
+border-bottom: #ddd 1px dotted;
+margin-right:5px
+}
+</style>
